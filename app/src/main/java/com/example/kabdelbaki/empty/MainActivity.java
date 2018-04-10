1baf8fac82419e0c7aa5608234bdc8fade866133
karimsoti <karim.abdelabki@soti.net>
2018-04-10 09:48:02 -0400

    Second commit, contains toast message

--- app/src/main/java/com/example/kabdelbaki/empty/MainActivity.java
+++ app/src/main/java/com/example/kabdelbaki/empty/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kabdelbaki.empty;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,11 +28,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        final Button button = findViewById(R.id.button1);                       //instantiate a button which finds a view using the Id of the button declared in activity_main.xml
+     /*   final Button button = findViewById(R.id.button1);                       //instantiate a button which finds a view using the Id of the button declared in activity_main.xml
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Paul is awesome",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Paul is awesome", Toast.LENGTH_LONG).show();
 
+            }
+        });*/
+
+        final Button popup = findViewById(R.id.popupBtn);                       //instantiate a button which finds a view using the Id of the button declared in activity_main.xml
+        popup.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+//
+                Intent myIntent = new Intent(getApplicationContext(), LoginActvitity.class);
+                startActivity(myIntent);
 
 
             }
