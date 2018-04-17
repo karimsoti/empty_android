aae20ee60a24b039e195198d89bc3dbcd9b8a0a7
karimsoti <karim.abdelabki@soti.net>
2018-04-17 19:39:20 -0400

    Second commit, contains toast message

--- app/src/main/java/com/example/kabdelbaki/empty/MainActivity.java
+++ app/src/main/java/com/example/kabdelbaki/empty/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.kabdelbaki.empty;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.PopupWindow;
@@ -21,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private Button mButton;
 
     private PopupWindow mPopupWindow;
+    private static final String TAG = "MyVerboseLog";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,17 +45,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });*/
 
-        final Button popup = findViewById(R.id.popupBtn);                       //instantiate a button which finds a view using the Id of the button declared in activity_main.xml
-        popup.setOnClickListener(new View.OnClickListener() {
+//        final Button popup = findViewById(R.id.popupBtn);                       //instantiate a button which finds a view using the Id of the button declared in activity_main.xml
+//        popup.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View view) {
+////
+//                Intent myIntent = new Intent(getApplicationContext(), LoginActvitity.class);
+//                startActivity(myIntent);
+//
+//
+//
+//
+//            }
+//        });
+
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+
+        final Button locationData = findViewById(R.id.locationBtn);                       //instantiate a button which finds a view using the Id of the button declared in activity_main.xml
+        locationData.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
 //
-                Intent myIntent = new Intent(getApplicationContext(), LoginActvitity.class);
-                startActivity(myIntent);
+                Log.v(TAG, "hello");
 
 
             }
         });
 
+    }
+
+    public void getLocation() {
+
+
+//    return"string";
     }
 
 
