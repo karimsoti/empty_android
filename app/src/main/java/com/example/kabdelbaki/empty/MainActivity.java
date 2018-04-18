7ce567233e87da10a5682cdf7fc7c695774a0f1f
karimsoti <karim.abdelabki@soti.net>
2018-04-17 21:39:33 -0400

    Location coordinates to log buffer

--- app/src/main/java/com/example/kabdelbaki/empty/MainActivity.java
+++ app/src/main/java/com/example/kabdelbaki/empty/MainActivity.java
@@ -10,6 +10,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +19,8 @@ import android.widget.Button;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import javax.xml.datatype.Duration;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
 //        final Button popup = findViewById(R.id.popupBtn);                       //instantiate a button which finds a view using the Id of the button declared in activity_main.xml
 //        popup.setOnClickListener(new View.OnClickListener() {
 //            public void onClick(View view) {
-////
+//
 //                Intent myIntent = new Intent(getApplicationContext(), LoginActvitity.class);
 //                startActivity(myIntent);
 //
@@ -58,24 +61,70 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-
 
         final Button locationData = findViewById(R.id.locationBtn);                       //instantiate a button which finds a view using the Id of the button declared in activity_main.xml
         locationData.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
 //
-                Log.v(TAG, "hello");
-
+//                Log.v(TAG, "hello");
+                getLocationEngine();
 
             }
         });
 
     }
 
-    public void getLocation() {
+
+    public void getLocationEngine() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            } else {
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+            }
+        } else {
 
 
+            LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            final double[] longitude = {location.getLongitude()};
+            final double[] latitude = {location.getLatitude()};
+
+            final LocationListener locationListener = new LocationListener() {
+                public void onLocationChanged(Location location) {
+                    longitude[0] = location.getLongitude();
+                    latitude[0] = location.getLatitude();
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+
+
+            };
+
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
+//            Toast.makeText(getApplicationContext(), longitude[0] + " " + latitude[0], Toast.LENGTH_SHORT).show();
+
+
+            Log.v(TAG, longitude[0] + " " + latitude[0]);
+        }
 //    return"string";
     }
 
